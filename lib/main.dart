dbc576772360823e68bfa20e0d29edd50fb1203a
vadiyagnik <vadiyagnik@gmail.com>
2022-06-01 08:33:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:login_id/view_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: view_page(),));
+
+}
+
